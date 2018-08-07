33cc03b6292121ed5d31052392c11b7dd6c6fa8c
kokhole.lim@selectv.co <kokhole.lim@selectv.co>
2018-08-07 21:29:41 +0800

    fixed missing comma

--- pypub/chapter.py
+++ pypub/chapter.py
@@ -31,7 +31,7 @@ class ImageErrorException(Exception):
 
 def get_image_type(url):
     url = url.lower()
-    for ending in ['jpg', 'jpeg', '.gif' '.png']:
+    for ending in ['jpg', 'jpeg', '.gif', '.png']:
         if url.endswith(ending):
             return ending
     else:
